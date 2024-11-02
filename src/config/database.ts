34edbe041c6cc777a67d03ee6427cba8b6674fc6
Abdessattar Louhichi <louhichi.abdessattar@yahoo.com>
2024-11-02 22:37:51 +0100

    config

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,11 +1,14 @@
 import { Sequelize } from "sequelize-typescript";
+import dotenv from "dotenv";
 
-const sequelize = new Sequelize({
-    database: 'school_management',
-    dialect: 'mysql',
-    port:3306,
-    username: 'root', 
-    password: '/*@Root@1988*/',
+dotenv.config();
+
+const sequelize = new Sequelize ({
+    database: process.env.DATA_BASE_NAME!,
+    dialect: process.env.DB_DIALECT as any,
+    port:process.env.PORT ? parseInt(process.env.PORT, 10) : 3306,
+    username: process.env.DB_USERNAME!, 
+    password: process.env.DB_PW!,
     models: [__dirname + '/../models']
 
 })
